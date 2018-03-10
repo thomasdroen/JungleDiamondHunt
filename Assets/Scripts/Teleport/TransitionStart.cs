18159dfd2740750d5c6ff16882640b58d6f391cf
ZYRGEL-DESKTOP <fredrhkv@stud.ntnu.no>
2018-03-10 14:06:36 +0100

    implemented a funcional teleport

---
+++ Assets/Scripts/Teleport/TransitionStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionStart : MonoBehaviour
+{
+
+    public Transform teleportLocation;
+    public bool interactable;
+
+    public void onPlayerInteract(GameObject player)
+    {
+        if (interactable)
+        {
+            player.transform.position = teleportLocation.position;
+        }
+    }
+}
