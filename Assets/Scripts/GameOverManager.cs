286661d22dcf894167fe16505a4be5ebdaf79544
Anders S <anders.smordal@hotmail.com>
2018-03-22 13:52:56 +0100

    Almost compleated the victory screen, some minor fixes to do. Added music when the player wins.

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour {
+
+	public float restartedDelay =5f;
+	public DiamondManager diamond;
+
+	public MusicChanger musicChanger; 
+
+	Animator anim;
+	float restartTimer;
+
+	void Awake(){
+		anim = GetComponent<Animator> ();
+	}
+
+	void OnEnable(){
+		diamond.onEnter += gameOver;	
+	}
+
+	void OnDisable(){
+
+		diamond.onEnter -= gameOver;
+	
+	}
+
+
+	void gameOver(){
+
+
+		
+
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+		musicChanger.EndGame ();
+		anim.SetTrigger ("GameOver");
+		//Time.timeScale = 0f;
+
+
+
+		/*restartTimer += Time.deltaTime;
+
+		if(restartTimer >= restartedDelay){
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}*/
+	}
+
+	public void goToMainMenu(){
+		musicChanger.Pause ();
+		SceneManager.LoadScene ("MainMenu");
+
+	}
+}
