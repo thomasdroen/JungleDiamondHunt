8b48e1f5999dc36768003c1971b624e783877718
Mikal <mikalhjorungdal@yahoo.no>
2018-04-11 20:21:53 +0200

    Added sound effects

---
+++ Assets/Scripts/LionPickupLine.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LionPickupLine : MonoBehaviour
+{
+    private string theCollider;
+
+    void OnTriggerEnter(Collider other)
+    {
+        theCollider = other.tag;
+        if (theCollider == "Player")
+        {
+            GetComponent<AudioSource>().Play();
+            GetComponent<AudioSource>().loop = false;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        theCollider = other.tag;
+        if (theCollider == "Player")
+        {
+            GetComponent<AudioSource>().Stop();
+            GetComponent<AudioSource>().loop = false;
+        }
+    }
+}
---
+++ Assets/Scripts/WaterAudioZone.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterAudioZone : MonoBehaviour
+{
+    private string theCollider;
+
+    void OnTriggerEnter(Collider other)
+    {
+        theCollider = other.tag;
+        if (theCollider == "Player")
+        {
+            GetComponent<AudioSource>().Play();
+            GetComponent<AudioSource>().loop = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        theCollider = other.tag;
+        if (theCollider == "Player")
+        {
+            GetComponent<AudioSource>().Stop();
+            GetComponent<AudioSource>().loop = false;
+        }
+    }
+}
